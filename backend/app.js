ac3645a3ca7ec25339d3e5a47c99bd83a46e2165
yovu <7omatoe.s@gmail.com>
2024-10-03 00:35:08 +0300

    im going backend insane

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,8 @@ const app = express()
 
 const PORT = 3000
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
