6f28266e7a8c822f55137150e5c4eec5a0312af6
yovu <7omatoe.s@gmail.com>
2024-10-03 13:01:41 +0300

    attempt stat

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const { verifyKeyMiddleware } = require('discord-interactions');
 const path = require('path');
-
 const app = express();
 const PORT = 3000;
 
 // Define the parent directory path
 const parentDir = path.join(__dirname, '..');
 
-// Middleware for serving static files
-app.use(express.static(path.join(parentDir, 'public_html')));
-app.use(express.json()); // Middleware for parsing JSON requests
+// Middleware for parsing JSON requests
+app.use(express.json());
 
 // Define the Discord interaction route
-app.get('/api/discord-auth', verifyKeyMiddleware('aea759dc26e8c1778a99e67e32cf261489fbba6e53e3139f9822f34bf47df1ee'), (req, res) => {
+app.post('/api/discord-auth', verifyKeyMiddleware('aea759dc26e8c1778a99e67e32cf261489fbba6e53e3139f9822f34bf47df1ee'), (req, res) => {
     const interaction = req.body;
     if (interaction.type === 1) {
         return res.json({ type: 1 }); // Respond with type 1
     }
     return res.json({ type: 4, data: { content: 'test' } }); // Respond with type 4
 });
+
+// Middleware for serving static files
+app.use(express.static(path.join(parentDir, 'public_html')));
 
 // Serve the index.html for the root path
 app.get('/', (req, res) => {
