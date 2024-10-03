387729090d8e0a6996f3f2b09bd3e207aadf37fe
yovu <7omatoe.s@gmail.com>
2024-10-03 18:31:33 +0300

    app test

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const path = require('path');
-const apiRouter = require('./routes'); // Adjust the path as necessary
-
 const app = express();
 const PORT = 3000;
 
-// Define the root directory path
-const rootDir = path.join(__dirname, '..');
+const rootDir = path.join(__dirname, '..'); 
+app.use(express.static(path.join(rootDir, 'public')));
 
-// Use the router for API endpoints
-app.use('api', apiRouter);
+app.get('/api/test', (req, res) => {
+    res.send('hi');
+});
 
-// Static file serving
-app.use(express.static(path.join(rootDir, 'public_html')));
+app.get('/api/discord-auth', (req, res) => {
+    res.json(1);
+});
 
-// Start the server
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server listening at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
