ac497a81ba3f74d55645728cc19c7b291db412d3
yovu <7omatoe.s@gmail.com>
2024-10-03 11:31:24 +0300

    json reply

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,10 @@ const PORT = 3000
 
 app.get('/api/discord-auth', (req, res) => {
     res.send('Hello World! test test test')
+    if (interaction.type === 1) {
+        res.send(res.json({ type: 1 }));
+      }
+      res.send(res.json({ type: 4, data: { content: 'test' } }));
 })
 
 app.listen(PORT, '0.0.0.0', () => {
